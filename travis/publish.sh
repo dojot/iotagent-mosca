78a5ea1fee3c86e82a20e2e46e61881152256b9a
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-07-06 20:56:06 -0300

    ci: fix travis publish.sh script.

--- travis/publish.sh
+++ travis/publish.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH != "master" ] ; then
   version=$TRAVIS_BRANCH
 fi
 
-tag=dojot/iotagent-mosca:$version
+tag=${TRAVIS_REPO_SLUG}:$version
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker tag ${TRAVIS_REPO_SLUG} ${tag}
