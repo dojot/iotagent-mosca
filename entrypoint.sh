7cf5c8e05e04fe9e2e94a472c3583edd19cbf1a6
Rafael Augusto Scaraficci <raugusto@cpqd.com.br>
2018-12-26 13:30:13 -0200

    Fixed service's output and changed the way the docker is upgraded
       - Fixed the output logged when the service runs without TLS.
    
       - Changed to upgrade docker used by Travis through addson syntax.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "${MOSCA_TLS}" = "true" ]; then
     echo "Generating keys and certificates for TLS..."
     /opt/iot-agent/initialConf.py && echo "TLS configured." || { echo "Error on TLS setup"; exit 1; }
 else 
-    echo "No TLS is configured. All connections are not unsecured."
+    echo "No TLS is configured. All connections are unsecured."
 fi
 
 echo "Starting iotagent-mosca..."
